423eaba291695f1e0fb917786b2ff2b1d54dcfc7
elijahmanda <99589143+elijahmanda@users.noreply.github.com>
2023-09-02 12:15:32 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class HelloWorld(App):
+    def build(self):
+        return Label(text="Hello World", halign="center", valign="center", font_size="32sp", bold=True)
+
+if __name__ == "__main__":
+    HelloWorld().run()
